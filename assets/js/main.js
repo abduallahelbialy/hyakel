3b7b50756ddcfbcc444b67e7b121d8a19aa4eead
abduallahelbialy <abduallahelbialy@gmail.com>
2025-04-07 10:27:48 +0200

    add pay page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
+///menu-toggle///
 const toggle = document.getElementById("menu-toggle");
 const navbar = document.getElementById("navbarNav");
 
 toggle.addEventListener("click", () => {
   navbar.classList.toggle("hidden");
 });
+
+///accordion////
+    document.querySelectorAll(".accordion-button").forEach((button) => {
+      button.addEventListener("click", function () {
+        document.querySelectorAll(".accordion-title").forEach((title) => {
+          title.style.color = "#030303";
+        });
+
+        const title = this.querySelector(".accordion-title");
+        if (this.getAttribute("aria-expanded") === "true") {
+          title.style.color = "orange";
+        }
+      });
+    });
+
