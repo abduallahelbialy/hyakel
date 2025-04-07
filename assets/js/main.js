280dfaf0d599d4104c2e94cdf46df32f2a82682b
abduallahelbialy <abduallahelbialy@gmail.com>
2025-04-07 23:20:11 +0200

    save code all

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,18 +21,18 @@ toggle.addEventListener("click", () => {
     });
 
 ///save code
-        // document.addEventListener("contextmenu", function (event) {
-        //   event.preventDefault();
-        // });
+        document.addEventListener("contextmenu", function (event) {
+          event.preventDefault();
+        });
 
-        // document.addEventListener("keydown", function (event) {
-        //   if (
-        //     event.key === "F12" ||
-        //     (event.ctrlKey && event.shiftKey && event.key === "I") ||
-        //     (event.ctrlKey && event.shiftKey && event.key === "J") ||
-        //     (event.ctrlKey && event.key === "U")
-        //   ) {
-        //     event.preventDefault();
-        //   }
-        // });
+        document.addEventListener("keydown", function (event) {
+          if (
+            event.key === "F12" ||
+            (event.ctrlKey && event.shiftKey && event.key === "I") ||
+            (event.ctrlKey && event.shiftKey && event.key === "J") ||
+            (event.ctrlKey && event.key === "U")
+          ) {
+            event.preventDefault();
+          }
+        });
 
