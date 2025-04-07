4da71b13ec36eadc7a9b13bc2d7a6f0fc01a70c3
abduallahelbialy <abduallahelbialy@gmail.com>
2025-04-07 23:19:45 +0200

    mx-auto

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,20 +19,20 @@ toggle.addEventListener("click", () => {
         }
       });
     });
-    
-///save code
-        document.addEventListener("contextmenu", function (event) {
-          event.preventDefault();
-        });
 
-        document.addEventListener("keydown", function (event) {
-          if (
-            event.key === "F12" ||
-            (event.ctrlKey && event.shiftKey && event.key === "I") ||
-            (event.ctrlKey && event.shiftKey && event.key === "J") ||
-            (event.ctrlKey && event.key === "U")
-          ) {
-            event.preventDefault();
-          }
-        });
+///save code
+        // document.addEventListener("contextmenu", function (event) {
+        //   event.preventDefault();
+        // });
+
+        // document.addEventListener("keydown", function (event) {
+        //   if (
+        //     event.key === "F12" ||
+        //     (event.ctrlKey && event.shiftKey && event.key === "I") ||
+        //     (event.ctrlKey && event.shiftKey && event.key === "J") ||
+        //     (event.ctrlKey && event.key === "U")
+        //   ) {
+        //     event.preventDefault();
+        //   }
+        // });
 
